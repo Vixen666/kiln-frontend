029334d0ca468632923726906e6b8c2733d5dc8c
HoganasKakel <johannes.roos@cchoganas.se>
2025-02-25 22:19:06 +0100

    First Build

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 const config = {
-  apiUrl: "http://192.168.0.21:5000",
-  //apiUrl: "http://83.254.139.178:5000",
-  // Add other global configuration settings here
+  apiUrl: process.env.REACT_APP_API_URL
 };
 
 export default config;
